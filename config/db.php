854840d680d84b8604de5ebe1d8cc86b673606f0
csynoers <3s0c9m7@gmail.com>
2019-07-01 23:42:56 +0700

    add koneksi mysqli

--- config/db.php
+++ config/db.php
@@ -1,4 +1,9 @@
 <?php
+	/* koneksi mysql */
 	mysql_connect("localhost","smpseday_sia","q2GV8&t!ikRW","smpseday_sia")or die("Gagal Koneksi");
 	mysql_select_db("smpseday_sia")or die("Tidak ada Database");
+
+	/* koneksi mysqli */
+	$connect = mysqli_connect("localhost","smpseday_sia","q2GV8&t!ikRW","smpseday_sia");
+	if (!$connect) die ("koneksi gagal:". mysqli_connect_error());
 ?>
