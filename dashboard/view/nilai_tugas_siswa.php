d31f5a2497ce35eaf79e5e47aa392504618ef2bd
csynoers <3s0c9m7@gmail.com>
2019-07-03 11:15:53 +0700

    debug

--- dashboard/view/nilai_tugas_siswa.php
+++ dashboard/view/nilai_tugas_siswa.php
@@ -109,63 +109,56 @@
     <?php 
     }
     else {
-    ?>
-    <div class="large-12 columns">
-        <div class="box">
-            <div class="box-header bg-transparent">
-                <!-- tools box -->
-                <div class="pull-right box-tools">
-
-                    <span class="box-btn" data-widget="collapse"><i class="icon-minus"></i>
-                    </span>
-                    <span class="box-btn" data-widget="remove"><i class="icon-cross"></i>
-                    </span>
+        echo '
+            <div class="large-12 columns">
+                <div class="box">
+                    <div class="box-header bg-transparent">
+                        <!-- tools box -->
+                        <div class="pull-right box-tools">
+        
+                            <span class="box-btn" data-widget="collapse"><i class="icon-minus"></i>
+                            </span>
+                            <span class="box-btn" data-widget="remove"><i class="icon-cross"></i>
+                            </span>
+                        </div>
+                        <h3 class="box-title"><i class="fontello-th-large-outline"></i>
+                            <span>Cari Data Nilai Tugas</span>
+                        </h3>
+                    </div>
+                    <!-- end /.box-header -->
+                    <div class="box-body small-5" style="display: block;">
+                        <form data-abide method="POST" action="" role="form" enctype="multipart/form-data">                 
+                            <div class="form-group"> 
+                                <label>Mata Pelajaran</label>
+                                <select name="pelajaran" class="form-control" required>';
+                                    $iduser = $_SESSION['id'];
+                                    $pelajaran  =   mysql_query("SELECT * FROM pelajaran, kelas WHERE kelas.kelas_id=pelajaran.kelas_id AND pelajaran.users_id='{$iduser}'");
+                                    while ($row=mysql_fetch_assoc($pelajaran))
+                                    {
+                                        echo '<option value="'.$row['pelajaran_id'].'"> '.$row['pelajaran_nama'].' Kelas ('.$row['kelas_nama'].')</option>';
+                                    }
+                                    echo '
+                                </select>
+                            </div>
+                            <div class="name-field"> 
+                                <label>Tahun Ajaran</label>
+                                <select name="tahun" class="form-control" required>';
+                                    $tahun  =   mysql_query("SELECT * FROM tahun");
+                                    while ($row=mysql_fetch_assoc($tahun))
+                                    {
+                                        echo '<option value="'.$row['tahun_id'].'"> '.$row['tahun_nama'].'</option>';
+                                    }
+                                    echo '
+                                </select>
+                            </div>
+                            <button class="btn btn-primary" type="submit" name="cek-nilai"><span class="fa fa-search"></span> Cari Nilai Tugas</button>
+                        </form>
+                    </div>
+                    <!-- end /.box-body -->
                 </div>
-                <h3 class="box-title"><i class="fontello-th-large-outline"></i>
-                    <span>Cari Data Nilai Tugas</span>
-                </h3>
+                <!-- end /.box -->
             </div>
-            <!-- end /.box-header -->
-            <div class="box-body small-5" style="display: block;">
-                <form data-abide method="POST" action="" role="form" enctype="multipart/form-data">                 
-                    <div class="form-group"> 
-                        <label>Mata Pelajaran</label>
-                        <select name="pelajaran" class="form-control" required>
-                            <?php 
-                            $iduser = $_SESSION['id'];
-                                $pelajaran  =   mysql_query("SELECT * FROM pelajaran, kelas WHERE kelas.kelas_id=pelajaran.kelas_id AND pelajaran.users_id='iduser'");
-
-                                while ($row=mysql_fetch_assoc($pelajaran)) {
-                            ?>
-                                <option value="<?php echo $row['pelajaran_id']; ?>"><?php echo $row['pelajaran_nama']; ?> Kelas (<?php echo $row['kelas_nama']; ?>)</option>
-                            <?php
-                                }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="name-field"> 
-                        <label>Tahun Ajaran</label>
-                        <select name="tahun" class="form-control" required>
-                            <?php 
-                                $tahun  =   mysql_query("SELECT * FROM tahun");
-
-                                while ($row=mysql_fetch_assoc($tahun)) {
-                            ?>
-                                <option value="<?php echo $row['tahun_id']; ?>"><?php echo $row['tahun_nama']; ?></option>
-                            <?php
-                                }
-                            ?>
-                        </select>
-                    </div>
-                    <button class="btn btn-primary" type="submit" name="cek-nilai"><span class="fa fa-search"></span> Cari Nilai Tugas</button>
-                </form>
-            </div>
-            <!-- end /.box-body -->
-        </div>
-        <!-- end /.box -->
-    </div>
-    <!-- end /.large-12 -->
-
-    <?php
+            <!-- end /.large-12 -->
+        ';
     }
 
