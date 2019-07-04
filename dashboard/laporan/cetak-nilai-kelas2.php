5b2ca10c7e292020b5b5954edaf1508f879b2340
csynoers <3s0c9m7@gmail.com>
2019-07-04 19:51:14 +0700

    debug

--- dashboard/laporan/cetak-nilai-kelas2.php
+++ dashboard/laporan/cetak-nilai-kelas2.php
@@ -12,6 +12,9 @@
     WHERE 1=1
         AND pelajaran_id='{$_POST["pelajaran"]}'
     ";
+
+    /* mendapatkan nama guru */
+    $row_guru= $_SESSION;
     echo '<pre>';
     print_r($_POST);
     print_r($_SESSION);
@@ -28,53 +31,6 @@
 		<link rel="shortcut icon" href="../images/favicon.png">
 
 		<title>Informasi Nilai Kelas</title>
-		
-		<!--======================== Bootstrap core CSS ========================== -->
-		<link href="../js/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="../js/jquery.gritter/css/jquery.gritter.css" />
-
-		<link rel="stylesheet" href="../fonts/font-awesome-4/css/font-awesome.min.css">
-
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-		<script src="../../assets/js/html5shiv.js"></script>
-		<script src="../../assets/js/respond.min.js"></script>
-		<![endif]-->
-		<link rel="stylesheet" type="text/css" href="../js/jquery.nanoscroller/nanoscroller.css" />
-		<link rel="stylesheet" type="text/css" href="../js/jquery.easypiechart/jquery.easy-pie-chart.css" />
-		<link rel="stylesheet" type="text/css" href="../js/bootstrap.switch/bootstrap-switch.css" />
-		<link rel="stylesheet" type="text/css" href="../js/bootstrap.datetimepicker/css/bootstrap-datetimepicker.min.css" />
-		<link rel="stylesheet" type="text/css" href="../js/jquery.select2/select2.css" />
-		<link rel="stylesheet" type="text/css" href="../js/bootstrap.slider/css/slider.css" />
-		<link rel="stylesheet" type="text/css" href="../js/jquery.niftymodals/css/component.css" />
-		<link rel="stylesheet" type="text/css" href="../js/jquery.datatables/bootstrap-adapter/css/datatables.css" />
-
-		<!--======================== Custom styles for this template ========================== -->
-		<link href="../css/style.css" rel="stylesheet" />
-		<script type="text/javascript">
-			function ajaxrunning()
-			{
-				if (window.XMLHttpRequest)
-				{
-					xmlhttp=new XMLHttpRequest();
-				}
-				else
-				{
-					xmlhttp =new ActiveXObject("Microsoft.XMLHTTP");
-				}
-		
-				xmlhttp.onreadystatechange=function()
-				{
-					if (xmlhttp.readyState==4 && xmlhttp.status==200)
-					{
-					}
-				}
-		
-				xmlhttp.open("GET","sms.php");
-				xmlhttp.send();
-				setTimeout("ajaxrunning()", 5000);
-			}
-		</script>
 	</head>
 <body>
 <center>
