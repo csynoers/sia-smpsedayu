bdf971dd337cdcfff076e77c0c2ed890f750fee0
csynoers <3s0c9m7@gmail.com>
2019-07-04 10:47:36 +0700

    debug

--- dashboard/laporan/cetak-nilai-kelas.php
+++ dashboard/laporan/cetak-nilai-kelas.php
@@ -14,9 +14,6 @@
 		
 		<!--======================== Bootstrap core CSS ========================== -->
 		<link href="../assets/bootstrap/css/bootstrap.css" rel="stylesheet">
-
-		<!--======================== Custom styles for this template ========================== -->
-		<link href="../assets/bootstrap/css/style.css" rel="stylesheet" />
 	</head>
 	<body>
 		<div id="DivIdToPrint" class="container">
@@ -25,80 +22,73 @@
 					<div class="page-head">
 						<h2>Laporan Nilai Tugas Siswa</h2><br>
 						<h4>SMPN 1 SEDAYU</h4>
-					</div>    
-					<div class="cl-mcont">
-						<!-- ===================================== Content Here ===================================== -->
-						<div class="block-flat no-padding">
-							<div class="content">
-								<table class="no-border blue">
-									<thead class="no-border">
-										<tr>
-											<th style="width: 5%;" class="text-center">No</th>
-											<th width="8%">Nama</th>
-											<th width="5%">Kelas</th>
-											<th width="7%">Mata Pelajaran</th>
-									
-											<th width="7%">Tahun</th>
-											<th style="width: 7%;" class="text-center">Nilai Poin</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php 
-											if (isset($_POST['cetak-nilai'])) {
-												$no 		=	1;
-												$pelajaran 	=	$_POST['pelajaran'];
-												$jenis 		=	$_POST['jenis'];
-												$tahun 		=	$_POST['tahun'];
-												$sql 		=	mysql_query("
-													SELECT
-														users.users_nama,
-														pelajaran.pelajaran_nama,
-														nilai.nilai_poin,
-														jenis.jenis_nama,
-														tahun.tahun_nama,
-														kelas.kelas_id,
-														kelas.kelas_nama 
-													FROM nilai
-														INNER JOIN users
-															ON nilai.users_id=users.users_id
-														INNER JOIN kelas
-															ON users.kelas_id=kelas.kelas_id
-														INNER JOIN pelajaran
-															ON nilai.pelajaran_id=pelajaran.pelajaran_id
-														INNER JOIN jenis
-															ON nilai.jenis_id=jenis.jenis_id
-														INNER JOIN tahun
-															ON nilai.tahun_id=tahun.tahun_id
-													WHERE jenis.jenis_id='$jenis' 
-														AND tahun.tahun_id='$tahun' 
-														AND pelajaran.pelajaran_id='$pelajaran'
-														GROUP BY users.users_nama");
-												while ($data=mysql_fetch_array($sql))
-												{
-													echo '
-														<tr align="center">
-															<td class="text-center">'.$no.'</td>
-															<td align="left">'.$data['users_nama'].'</td>
-															<td>'.$data['kelas_nama'].'</td>
-															<td>'.$data['pelajaran_nama'].'</td>
-															<td>'.$data['tahun_nama'].'</td>
-															<td class="text-center">'.($data['nilai_poin'] == 0 ? "Data Kosong" : $data['nilai_poin'] ).'</td>
-														</tr>
-													';
-													$no++;  
-												}
-											}
-										?>
-									</tbody>
-								</table>
-								<hr/>
-								<a href="../?cetak=laporan-nilai-kelas" cls="btn" class="btn btn-info"><i class="fa fa-reply"></i> Kembali </a>
-								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								<a href="cetak-nilai-kelas.php" cls="btn" onclick="window.print();return false" class="btn btn-primary"><i class="fa fa-print"></i> Cetak </a>
-							</div>
-						</div>
-						<!-- ===================================== Content Here ===================================== -->
 					</div>
+				</div>
+				<div class="table-responsive">
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th >No</th>
+								<th >Nama</th>
+								<th >Kelas</th>
+								<th >Mata Pelajaran</th>
+								<th >Tahun</th>
+								<th >Nilai Poin</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php 
+								if (isset($_POST['cetak-nilai'])) {
+									$no 		=	1;
+									$pelajaran 	=	$_POST['pelajaran'];
+									$jenis 		=	$_POST['jenis'];
+									$tahun 		=	$_POST['tahun'];
+									$sql 		=	mysql_query("
+										SELECT
+											users.users_nama,
+											pelajaran.pelajaran_nama,
+											nilai.nilai_poin,
+											jenis.jenis_nama,
+											tahun.tahun_nama,
+											kelas.kelas_id,
+											kelas.kelas_nama 
+										FROM nilai
+											INNER JOIN users
+												ON nilai.users_id=users.users_id
+											INNER JOIN kelas
+												ON users.kelas_id=kelas.kelas_id
+											INNER JOIN pelajaran
+												ON nilai.pelajaran_id=pelajaran.pelajaran_id
+											INNER JOIN jenis
+												ON nilai.jenis_id=jenis.jenis_id
+											INNER JOIN tahun
+												ON nilai.tahun_id=tahun.tahun_id
+										WHERE jenis.jenis_id='$jenis' 
+											AND tahun.tahun_id='$tahun' 
+											AND pelajaran.pelajaran_id='$pelajaran'
+											GROUP BY users.users_nama");
+									while ($data=mysql_fetch_array($sql))
+									{
+										echo '
+											<tr align="center">
+												<td class="text-center">'.$no.'</td>
+												<td align="left">'.$data['users_nama'].'</td>
+												<td>'.$data['kelas_nama'].'</td>
+												<td>'.$data['pelajaran_nama'].'</td>
+												<td>'.$data['tahun_nama'].'</td>
+												<td class="text-center">'.($data['nilai_poin'] == 0 ? "Data Kosong" : $data['nilai_poin'] ).'</td>
+											</tr>
+										';
+										$no++;  
+									}
+								}
+							?>
+						</tbody>
+					</table>
+					<hr/>
+					<a href="../?cetak=laporan-nilai-kelas" cls="btn" class="btn btn-info"><i class="fa fa-reply"></i> Kembali </a>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="cetak-nilai-kelas.php" cls="btn" onclick="window.print();return false" class="btn btn-primary"><i class="fa fa-print"></i> Cetak </a>
 				</div>
 			</center>
 		</div>
