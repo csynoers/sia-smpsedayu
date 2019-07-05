245a1be21112d042e88e276dd6ba8d08608e95ed
csynoers <3s0c9m7@gmail.com>
2019-07-05 17:10:07 +0700

    debug

--- dashboard/chat/chat_siswa.php
+++ dashboard/chat/chat_siswa.php
@@ -57,30 +57,11 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <section class="post-new-forum">
-        <div class="container">
-            <div class="panel panel-default">
-                <div class="panel-heading">Post New Forum</div>
-                <div class="panel-body">
-                    <form method="POST" action="simpan_pesan.php">
-						<input type="hidden" name="user_id" value="<?php echo $_SESSION['noinduk'] ?>">
-                        <div class="form-group">
-                            <textarea rows="5" name="post" class="form-control" placeholder="Isi Pesan Disini ..."></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-success">Kirim</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- end /.post-new-forum -->
 
     <section class="forums-content">
         <div class="container">
             <div class="panel panel-default">
-                <div class="panel-heading">Forum Anda</div>
+                <div class="panel-heading">Forum Diskusi Anda</div>
                 <div class="panel-body">
                     <?php
                         $sql= "
