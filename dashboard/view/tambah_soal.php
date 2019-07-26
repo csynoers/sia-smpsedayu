f757a2d7c3b701dd1d2cf36fe13c6df17811a5b2
csynoers <3s0c9m7@gmail.com>
2019-07-26 23:56:45 +0700

    debug

--- dashboard/view/tambah_soal.php
+++ dashboard/view/tambah_soal.php
@@ -31,38 +31,38 @@
 
                     <div class="name-field">
                     <label>Soal<small> required</small>
-                        <textarea name="soal" style="margin: 0px 0px 16px; width: 398px; height: 265px;" required><?php echo $row[0]['soal_kuis'] ?></textarea>
+                        <textarea name="soal" style="margin: 0px 0px 16px; width: 398px; height: 265px;" required></textarea>
                     </label>
                 </div>
                 <div class="name-field">
                     <label>jawaban A<small> required</small>
-                        <input value="<?php echo $row[0]['pil_a'] ?>" type="text" name="pil_a" required>
+                        <input type="text" name="pil_a" required>
                     </label>
 					</div>
 				<div class="name-field">
                     <label>jawaban B<small> required</small>
-                        <input value="<?php echo $row[0]['pil_b'] ?>" type="text" name="pil_b" required>
+                        <input type="text" name="pil_b" required>
                     </label>
 				</div>
 			<div class="name-field">
                     <label>Jawaban C<small> required</small>
-                        <input value="<?php echo $row[0]['pil_c'] ?>" type="text" name="pil_c" required>
+                        <input type="text" name="pil_c" required>
                     </label>
 			</div>
 			<div class="name-field">
                     <label>Jawaban D<small> required</small>
-                        <input value="<?php echo $row[0]['pil_d'] ?>" type="text" name="pil_d" required>
+                        <input type="text" name="pil_d" required>
                     </label>
 			</div>                    
             <div class="name-field" >
                     <label>Kunci Jawaban</label>
-	                    <input type="radio"   id="pokemonRed" name="kunci" value="A" <?php ($row[0]['kunci']=='A')? 'checked' : NULL ?> >
+	                    <input type="radio"   id="pokemonRed" name="kunci" value="A">
                         <label for="pokemonRed">A</label>
-                        <input type="radio"   id="pokemonBlue" name="kunci" value="B" <?php ($row[0]['kunci']=='B')? 'checked' : NULL ?> >
+                        <input type="radio"   id="pokemonBlue" name="kunci" value="B">
                         <label for="pokemonBlue">B</label>
-	                    <input type="radio"   id="pokemonRed" name="kunci" value="C" <?php ($row[0]['kunci']=='C')? 'checked' : NULL ?>>
+	                    <input type="radio"   id="pokemonRed" name="kunci" value="C">
                         <label for="pokemonRed">C</label>
-                        <input type="radio"   id="pokemonBlue" name="kunci" value="D" <?php ($row[0]['kunci']=='D')? 'checked' : NULL ?>>
+                        <input type="radio"   id="pokemonBlue" name="kunci" value="D">
                         <label for="pokemonBlue">D</label>
                  </div>
              </div>
