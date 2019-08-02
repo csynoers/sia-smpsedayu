ee6466d3c0f3dd91819089b5b486cc296283d880
csynoers <3s0c9m7@gmail.com>
2019-08-02 22:30:21 +0700

    debug

--- dashboard/controller/create.php
+++ dashboard/controller/create.php
@@ -1234,6 +1234,7 @@ if (isset($_POST['update_kelas_siswa'])) {
 				INNER JOIN users
 					ON users.users_id=pbm.user_id
 			WHERE users_id='{$value}'");
+		print_r($sql);
 		$query= query_result($connect, $sql)['fetch_assoc'][0];
 		if ( $query['count_pbm'] > 0 ) {
 			array_push($gagal, "{$query['users_nama']} ({$query['users_noinduk']})");
