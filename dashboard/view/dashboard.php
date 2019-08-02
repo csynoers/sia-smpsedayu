2a8ae91c67805902a328b22e6e387c32fc61a40e
csynoers <3s0c9m7@gmail.com>
2019-08-02 10:42:19 +0700

    debug

--- dashboard/view/dashboard.php
+++ dashboard/view/dashboard.php
@@ -1,4 +1,31 @@
 <?php
-    echo '<pre>';
-    print_r($_SESSION);
-    echo '</pre>';
+    $sql= ("SELECT * FROM users WHERE users_id = '{$_SESSION['id']}'");
+    foreach (query_result($connect, $sql)['fetch_assoc'] as $key => $value) {
+        echo '
+            <div class="large-12 columns">
+                <div class="box">
+                    <div class="box-header bg-transparent">
+                        <!-- tools box -->
+                        <div class="pull-right box-tools">
+            
+                            <span class="box-btn" data-widget="collapse"><i class="icon-minus"></i>
+                            </span>
+                            <span class="box-btn" data-widget="remove"><i class="icon-cross"></i>
+                            </span>
+                        </div>
+                        <h3 class="box-title"><i class="fontello-th-large-outline"></i>
+                            <span>Informasi Profil</span>
+                        </h3>
+                    </div>
+                    <!-- /.box-header -->
+            
+                    <div class="box-body small-5" style="display: block;">
+                        
+                    </div>
+                    <!-- end .timeline -->
+                </div>
+                <!-- box -->
+            </div>
+        ';
+    }
+?>
