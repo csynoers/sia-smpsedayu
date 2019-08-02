3192d015a18a6e6e692eac415d8c8c43135b60bb
csynoers <3s0c9m7@gmail.com>
2019-08-02 20:18:26 +0700

    debug

--- dashboard/controller/create.php
+++ dashboard/controller/create.php
@@ -1226,14 +1226,13 @@ if (isset($_POST['raport-proses'])) {
 if (isset($_POST['update_kelas_siswa'])) {
 	$sukses=[];
 	$gagal=[];
-	// foreach ($_POST['user_id'] as $key => $value) {
-	// 	$sql= ("SELECT * FROM pbm WHERE user_id='{$value}' AND kelas_id='{$_POST['kelas_id']}' AND tahun_id='{$_POST['tahun_id']}' ");
-	// 	// query_result($connect, $sql)['fetch_assoc'];
-	// 	echo '<pre>';
-	// 	print_r(query_result($connect, $sql));
-	// 	echo '</pre>';
-	// }
-	echo 'sdasdasd';
+	foreach ($_POST['user_id'] as $key => $value) {
+		$sql= ("SELECT * FROM pbm WHERE user_id='{$value}' AND kelas_id='{$_POST['kelas_id']}' AND tahun_id='{$_POST['tahun_id']}' ");
+		// query_result($connect, $sql)['fetch_assoc'];
+		echo '<pre>';
+		print_r(query_result($connect, $sql));
+		echo '</pre>';
+	}
 }
 ?>
 
