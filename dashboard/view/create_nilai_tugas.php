3d8e17d74a0c299f22332a9c76192ed12325e987
csynoers <3s0c9m7@gmail.com>
2019-08-06 01:07:18 +0700

    debug Nilai: Input Nilai Tugas 15

--- dashboard/view/create_nilai_tugas.php
+++ dashboard/view/create_nilai_tugas.php
@@ -44,83 +44,93 @@
 	</div>
 </div>
 
-
-
 <div class="large-12 columns">
 	<div class="box">
-		<table class="box-header bg-transparent"  style="width:100%">
-			<thead class="no-border">
-				<tr>
-					<th style="width: 5%;" class="text-center">No</th>
-					<th style="width: 5%;" class="text-center">NIS</th>
-					<th >Nama</th>
-					<th style="width: 17%;" class="text-center">Nilai Poin</th>
-				</tr>
-			</thead>	
-			<form role="form" method="post" >
-				<?php
-					if(isset($_POST['create-nilai'])) {
-						
-						$no 			= 1;
-						// $kelasnama 		= $_POST['kelas'];
-						// $pelajarannama 	= $_POST['pelajaran'];
-						// $semesternama 	= $_POST['semester'];
-						// $jenisnama		= '1';
-						// $tahunnama 		= $_POST['tahun'];
-						$sql 			= ("
-							SELECT
-								users.users_id,
-								users.users_noinduk,
-								users.users_nama,
-								pbm_o.kelas_id,
-								(
-									SELECT pbm.kelas_id
-									FROM pbm
-									WHERE pbm.user_id=pbm_o.user_id
-									ORDER BY pbm.pbm_id DESC LIMIT 1
-								) AS kelas_id_mod
-							FROM pbm AS pbm_o
-								INNER JOIN users
-									ON users.users_id=pbm_o.user_id
-							WHERE 1=1
-								AND pbm_o.kelas_id= (
-									SELECT pelajaran.kelas_id
-										FROM pelajaran
-									WHERE 1=1
-										AND pelajaran.pelajaran_id='{$_POST['pelajaran']}'
-									)
-								HAVING kelas_id_mod=pbm_o.kelas_id
-							ORDER BY users.users_nama ASC 
-						");
-						foreach ( query_result($connect, $sql)['fetch_assoc'] as $key => $data) {
-					?>
-				<tr>
-					<td class="text-center"><?php echo $no; ?></td>
-					<td class="text-center"><?php echo $data['users_noinduk']; ?></td>
-					<td>
-						<input type="hidden" class="form-control" name="users[]" id="users[]" value="<?php echo $data['users_id']; ?>">
-						<?php 
-							echo $data['users_nama'];
+		<?php
+			if(isset($_POST['create-nilai'])) {
+				?>
+				<form role="form" method="post" >
+					<div class="form-group"> 
+						<label>Pilih Tugas</label>
+						<select name="tugas_id" class="form-control" required>
+							<?php
+								$sql = (" SELECT * FROM instgs WHERE instgs.pelajaran_id='{$_POST['pelajaran']}' ");
+								foreach ( query_result($connect, $sql)['fetch_assoc'] as $key => $value) {
+									echo '<option value="'.$value['instgs_id'].'" > '.$value['judul'].'</option>';
+								}
+							?>
+						</select>
+					</div>
+					<table class="box-header bg-transparent"  style="width:100%">
+						<thead class="no-border">
+							<tr>
+								<th style="width: 5%;" class="text-center">No</th>
+								<th style="width: 5%;" class="text-center">NIS</th>
+								<th >Nama</th>
+								<th style="width: 17%;" class="text-center">Nilai Poin</th>
+							</tr>
+						</thead>
+						<?php
+							$no 			= 1;
+							// $kelasnama 		= $_POST['kelas'];
+							// $pelajarannama 	= $_POST['pelajaran'];
+							// $semesternama 	= $_POST['semester'];
+							// $jenisnama		= '1';
+							// $tahunnama 		= $_POST['tahun'];
+							$sql 			= ("
+								SELECT
+									users.users_id,
+									users.users_noinduk,
+									users.users_nama,
+									pbm_o.kelas_id,
+									(
+										SELECT pbm.kelas_id
+										FROM pbm
+										WHERE pbm.user_id=pbm_o.user_id
+										ORDER BY pbm.pbm_id DESC LIMIT 1
+									) AS kelas_id_mod
+								FROM pbm AS pbm_o
+									INNER JOIN users
+										ON users.users_id=pbm_o.user_id
+								WHERE 1=1
+									AND pbm_o.kelas_id= (
+										SELECT pelajaran.kelas_id
+											FROM pelajaran
+										WHERE 1=1
+											AND pelajaran.pelajaran_id='{$_POST['pelajaran']}'
+										)
+									HAVING kelas_id_mod=pbm_o.kelas_id
+								ORDER BY users.users_nama ASC 
+							");
+							foreach ( query_result($connect, $sql)['fetch_assoc'] as $key => $data) {
+								?>
+									<tr>
+										<td class="text-center"><?php echo $no; ?></td>
+										<td class="text-center"><?php echo $data['users_noinduk']; ?></td>
+										<td>
+											<input type="hidden" class="form-control" name="users[]" id="users[]" value="<?php echo $data['users_id']; ?>">
+											<?php 
+												echo $data['users_nama'];
+											?>
+											<!-- <input type="hidden" name="pelajaran[]" id="pelajaran[]" value="<?php echo "$pelajarannama"; ?>"> -->
+											<!-- <input type="hidden" name="jenis[]" id="jenis[]" value="<?php echo "$jenisnama"; ?>"> -->
+											<!-- <input type="hidden" name="tahun[]" id="tahun[]" value="<?php echo "$tahunnama"; ?>"> -->
+										</td>
+										<td width="144" class="text-center">
+											<input type="text" class="form-control" name="nilai[]" id="nilai[]" />
+										</td>
+									</tr>
+								<?php
+								$no++;
+							}
 						?>
-						<!-- <input type="hidden" name="pelajaran[]" id="pelajaran[]" value="<?php echo "$pelajarannama"; ?>"> -->
-						<!-- <input type="hidden" name="jenis[]" id="jenis[]" value="<?php echo "$jenisnama"; ?>"> -->
-						<!-- <input type="hidden" name="tahun[]" id="tahun[]" value="<?php echo "$tahunnama"; ?>"> -->
-					</td>
-					<td width="144" class="text-center">
-						<input type="text" class="form-control" name="nilai[]" id="nilai[]" />
-					</td>
-				</tr>
-				<?php 
-					$no++;  
-					}
-				?>		
-				<tr>
-					<td colspan="3" align="right" valign="baseline"><button type="submit" class="btn btn-success" name="nilai-tugas-create"><span class="fontello-minefield"></span> Proses</button></td>
-				</tr>
-			</form>
-			<?php 
-				}
-			?>
-		</table>
+						<tr>
+							<td colspan="3" align="right" valign="baseline"><button type="submit" class="btn btn-success" name="nilai-tugas-create"><span class="fontello-minefield"></span> Proses</button></td>
+						</tr>
+						</table>
+				</form>
+				<?php
+			}
+		?>
 	</div>
 </div>
