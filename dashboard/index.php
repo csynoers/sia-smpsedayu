f7684075d5b7ac1e622c3654ae2d64474f766fec
csynoers <3s0c9m7@gmail.com>
2019-08-05 12:34:13 +0700

    set timezone

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,6 +1,6 @@
 <!-- konfirmasi kedatabase -->
 <?php
-    
+    date_default_timezone_set('Asia/Jakarta');
     session_start();
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
