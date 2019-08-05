488515f78ae6302ee5a3a5934c76f0aa7d187557
csynoers <3s0c9m7@gmail.com>
2019-08-05 13:15:51 +0700

    update guru with or without image

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,9 +2,10 @@
 <?php
     date_default_timezone_set('Asia/Jakarta');
     session_start();
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    error_reporting(0);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
     if (!isset($_SESSION['username'])) {
         header('Location: ../index.php');
     }
