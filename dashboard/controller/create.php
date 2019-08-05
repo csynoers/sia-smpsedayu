869a25ccd5d79299248e43bc72b7f60ac34b9470
csynoers <3s0c9m7@gmail.com>
2019-08-06 01:36:36 +0700

    debug Nilai: Input Nilai Tugas 20

--- dashboard/controller/create.php
+++ dashboard/controller/create.php
@@ -837,58 +837,6 @@ if (isset($_POST['siswa-create'])) {
 ?>
 
 <?php 
-	
-	if (isset($_POST['galeri-create'])) {
-		$nama 		=	$_POST['nama'];
-		$folder 	= 	'galeri';
-        $tmp_name 	= 	$_FILES["foto"]["tmp_name"];
-        $link 		= 	$folder."/".$_FILES["foto"]["name"];
-
-		//Upload ke folder foto
-        move_uploaded_file($tmp_name, $link);
-
-		$galeri 		=	mysql_query("INSERT INTO galeri (`galeri_id`, `galeri_nama`, `galeri_link`) 
-									VALUES (NULL, '$nama', '$link')");
-
-		if ($galeri) {
-			echo "
-			<div class='large-12 columns'>
-				<div class='box bg-light-green'>
-					<div class='box-header bg-light-green'>
-						<div class='pull-right box-tools'>
-							<span class='box-btn' data-widget='remove'><i class='icon-cross'></i></span>
-						</div>
-						<h3 class='box-title '><i class='text-white  icon-thumbs-up'></i>
-							<span class='text-white'>SUCCESS</span>
-						</h3>
-					</div>
-					<div class='box-body ' style='display: block;'>
-						<p class='text-white'><strong>Well done!</strong> You successfully read this important alert message.</p>
-					</div>
-				</div>
-			</div>";
-			echo "<meta http-equiv='refresh' content='1;URL=?akademik=galeri'>";
-		}else {
-			echo "
-			<div class='large-12 columns'>
-				<div class='box bg-light-yellow'>
-					<div class='box-header bg-light-yellow'>
-						<div class='pull-right box-tools'>
-							<span class='box-btn' data-widget='remove'><i class='icon-cross'></i></span>
-						</div>
-						<h3 class='box-title '><i class='text-white  fontello-warning'></i>
-							<span class='text-white'>Warning</span>
-						</h3>
-					</div>
-					<div class='box-body ' style='display: block;'>
-						<p class='text-white'><strong>Warning!</strong> Best check yo self, you're not looking too good.</p>
-					</div>
-				</div>
-			</div>";
-			echo "<meta http-equiv='refresh' content='1;URL=?akademik=galeri'>";
-		}
-	}
-
 	if (isset($_POST['nilai-tugas-create'])) {
 		$value_sql= '';
 		foreach ($_POST['users'] as $key => $value) {
@@ -899,13 +847,13 @@ if (isset($_POST['siswa-create'])) {
 		die();
 
 
-	for($x=0;$x<$jumlahdata;$x++) {
+	/* for($x=0;$x<$jumlahdata;$x++) {
 			$tugas	=	mysql_query("INSERT INTO nilai
 										VALUES (NULL, '$siswa[$x]', '$pelajaran[$x]', '$jenis[$x]', '$tahun[$x]', '$nilaipoin[$x]')");	
 	
 	
+	} */
 	}
-}
 		
 ?>
 
