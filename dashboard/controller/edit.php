724a545d41ed90bd50af3481be7a482b8448e36b
csynoers <3s0c9m7@gmail.com>
2019-08-05 13:42:47 +0700

    Fix: update guru with image

--- dashboard/controller/edit.php
+++ dashboard/controller/edit.php
@@ -153,7 +153,6 @@ if (!empty($_FILES["file"]["tmp_name"]))
 						WHERE users_id = '{$id}'
 					");
 				}
-				print_r($sql);
 				$query= mysql_query($sql);
 				if ( $query ) {
 					echo "<script>alert('Data berhasil diubah'); window.history.go(-2);</script>";
