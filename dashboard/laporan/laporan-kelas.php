d9f702f6522ca9cb875c214f1ddd42cedb9c5ffb
csynoers <3s0c9m7@gmail.com>
2019-08-06 12:45:06 +0700

    Laporan nilai kuis

--- dashboard/laporan/laporan-kelas.php
+++ dashboard/laporan/laporan-kelas.php
@@ -17,12 +17,11 @@
                 $iduser     =$_SESSION['id'];  ?> 
     <div class="box-body small-5" style="display: block;">
 		<form target="_blank" data-abide method="POST" action="laporan/cetak-nilai-kelas.php" role="form" enctype="multipart/form-data"> 
-			 <div class="form-group"> 
-                 <label>Mata Pelajaran</label>
+			<div class="form-group"> 
+				<label>Mata Pelajaran</label>
                 <select name="pelajaran" class="form-control" required>
                     <?php 
-                    $iduser = $_SESSION['id'];
-                        $pelajaran  =   mysql_query("SELECT * FROM pelajaran, kelas WHERE pelajaran.kelas_id=kelas.kelas_id AND pelajaran.users_id='$iduser'");
+                        $pelajaran  =   mysql_query("SELECT *,(SELECT COUNT(nilai.nilai_id) FROM nilai WHERE nilai.pelajaran_id=pelajaran.pelajaran_id) count_nilai FROM pelajaran, kelas WHERE pelajaran.kelas_id=kelas.kelas_id AND pelajaran.users_id='{$_SESSION['id']}' HAVING count_nilai > 0");
 
                         while ($row=mysql_fetch_array($pelajaran)) {
                     ?>
@@ -34,14 +33,14 @@
             </div>
 		
 			<div class="name-field"> 
-				<label>Jenis Nilai</label>
-				<select name="jenis" class="form-control" required>
+				<label>Judul Tugas</label>
+				<select name="tugas_id" class="form-control" required>
 					<?php 
-						$jenis	=	mysql_query("SELECT * FROM jenis");
+						$jenis	=	mysql_query("SELECT *,(SELECT COUNT(nilai.nilai_id) FROM nilai WHERE nilai.instgs_id=instgs.instgs_id) count_nilai FROM pelajaran, kelas, instgs WHERE pelajaran.kelas_id=kelas.kelas_id AND instgs.pelajaran_id=pelajaran.pelajaran_id AND pelajaran.users_id={$_SESSION['id']} HAVING count_nilai > 0");
 
 						while ($row=mysql_fetch_array($jenis)) {
 					?>
-						<option value="<?php echo $row['jenis_id']; ?>"><?php echo $row['jenis_nama']; ?></option>
+						<option value="<?php echo $row['instgs_id']; ?>"><?php echo $row['judul']; ?></option>
 					<?php
 						}
 					?>
@@ -52,7 +51,7 @@
 				<label>Tahun Ajaran</label>
 				<select name="tahun" class="form-control" required>
 					<?php 
-						$tahun	=	mysql_query("SELECT * FROM tahun");
+						$tahun	=	mysql_query("SELECT *,IF(tahun.semester='1','Ganjil','Genap') AS semester_mod FROM pelajaran INNER JOIN nilai ON nilai.pelajaran_id=pelajaran.pelajaran_id LEFT JOIN tahun ON tahun.tahun_id=nilai.tahun_id WHERE 1=1 AND pelajaran.users_id={$_SESSION['id']} GROUP BY nilai.pelajaran_id,nilai.instgs_id");
 
 						while ($row=mysql_fetch_array($tahun)) {
 					?>
